0c4e80b0852a521303f75139b4db44ac893bf01d
Raine Soriano <rainearcher@g.ucla.edu>
2023-11-27 19:53:47 -0800

    Correct inode table

--- ext2-create.c
+++ ext2-create.c
@@ -360,9 +360,8 @@ void write_inode(int fd, u32 index, struct ext2_inode *inode) {
 	}
 }
 
-void write_inode_table(int fd) {
-	u32 current_time = get_current_time();
-
+void write_lost_and_found_inode(int fd, u32 current_time)
+{
 	struct ext2_inode lost_and_found_inode = {0};
 	lost_and_found_inode.i_mode = EXT2_S_IFDIR
 	                              | EXT2_S_IRUSR
@@ -383,9 +382,80 @@ void write_inode_table(int fd) {
 	lost_and_found_inode.i_blocks = 2; /* These are oddly 512 blocks */
 	lost_and_found_inode.i_block[0] = LOST_AND_FOUND_DIR_BLOCKNO;
 	write_inode(fd, LOST_AND_FOUND_INO, &lost_and_found_inode);
+}
 
-	// TODO It's all yours
-	// TODO finish the inode entries for the other files
+void write_root_inode(int fd, u32 current_time)
+{
+	struct ext2_inode root_inode = {0};
+	root_inode.i_mode = EXT2_S_IFDIR
+	                              | EXT2_S_IRUSR
+	                              | EXT2_S_IWUSR
+	                              | EXT2_S_IXUSR
+	                              | EXT2_S_IRGRP
+	                              | EXT2_S_IXGRP
+	                              | EXT2_S_IROTH
+	                              | EXT2_S_IXOTH;
+	root_inode.i_uid = 0;
+	root_inode.i_size = 1024;
+	root_inode.i_atime = current_time;
+	root_inode.i_ctime = current_time;
+	root_inode.i_mtime = current_time;
+	root_inode.i_dtime = 0;
+	root_inode.i_gid = 0;
+	root_inode.i_links_count = 3;
+	root_inode.i_blocks = 2; /* These are oddly 512 blocks */
+	root_inode.i_block[0] = ROOT_DIR_BLOCKNO;
+	write_inode(fd, EXT2_ROOT_INO, &root_inode);
+}
+
+void write_hello_world_file_inode(int fd, u32 current_time)
+{
+	struct ext2_inode hello_world = {0};
+	hello_world.i_mode = EXT2_S_IFREG
+	                              | EXT2_S_IRUSR
+	                              | EXT2_S_IWUSR
+	                              | EXT2_S_IRGRP
+	                              | EXT2_S_IROTH;
+	hello_world.i_uid = 1000;
+	hello_world.i_size = 12;
+	hello_world.i_atime = current_time;
+	hello_world.i_ctime = current_time;
+	hello_world.i_mtime = current_time;
+	hello_world.i_dtime = 0;
+	hello_world.i_gid = 1000;
+	hello_world.i_links_count = 1;
+	hello_world.i_blocks = 2; /* These are oddly 512 blocks */
+	hello_world.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
+	write_inode(fd, HELLO_WORLD_INO, &hello_world);
+}
+
+void write_hello_symlink_inode(int fd, u32 current_time)
+{
+	struct ext2_inode hello = {0};
+	hello.i_mode = EXT2_S_IFLNK
+	                              | EXT2_S_IRUSR
+	                              | EXT2_S_IWUSR
+	                              | EXT2_S_IRGRP
+	                              | EXT2_S_IROTH;
+	hello.i_uid = 1000;
+	hello.i_size = 12;
+	hello.i_atime = current_time;
+	hello.i_ctime = current_time;
+	hello.i_mtime = current_time;
+	hello.i_dtime = 0;
+	hello.i_gid = 1000;
+	hello.i_links_count = 1;
+	hello.i_blocks = 2; /* These are oddly 512 blocks */
+	strcpy((char *)hello.i_block, "hello-world");
+	write_inode(fd, HELLO_WORLD_INO, &hello);
+}
+
+void write_inode_table(int fd) {
+	u32 current_time = get_current_time();
+	write_root_inode(fd, current_time);
+	write_lost_and_found_inode(fd, current_time);
+	write_hello_world_file_inode(fd, current_time);
+	write_hello_symlink_inode(fd, current_time);
 }
 
 void write_root_dir_block(int fd)
